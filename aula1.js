17be57bf6a9f0c8d2ea75ee78d43030ecad12500
Felipe Andrade <138839013+felpsandrade@users.noreply.github.com>
2023-12-30 13:45:55 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,19 @@
+//var(variaveis): espaços na memoria que reservamos um valor.
+//let: valor de variavel que pode ser alterado no futuro.
+//const: valor de variavel que não pode ser alterado.
+
+let variavel1 = 10;
+
+console.log(variavel1, 'Primeiro Valor');
+
+/* operadores matematicos
++ (mais)
+- (menos)
+* (multiplicação)
+/ (divisão)
+% (resto da divisão)*/
+
+let variavel2 = 10 + 20;
+
+console.log(variavel2, 'Resultado dos Operadores');
+
